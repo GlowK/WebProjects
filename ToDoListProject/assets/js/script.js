2cb6188014e62681f397e33c3141892a5a774899
GlowK <glow.k@poczta.fm>
2019-05-03 21:25:21 +0200

    Section 22

--- ToDoListProject/assets/js/script.js
+++ ToDoListProject/assets/js/script.js
@@ -1,3 +1,27 @@
 // $("body").css("background-color","green");
 
-$("li").on()
+$("ul").on("click","li", function(){
+    $(this).toggleClass("completed");
+});
+
+//Clikc on X to delete "Todo"
+$("ul").on("click", "li span", function(event){
+    $(this).parent().fadeOut(500, function(e){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new value from the input
+        var todoText = $(this).val();
+        $(this).val("");
+        //adding new li in ul
+        $("ul").append("<li><span id='trashcan'><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>")
+    }
+})
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
