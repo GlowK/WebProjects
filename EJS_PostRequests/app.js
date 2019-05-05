8c2d933a8cd55f60ef9752712d6ac3fec2f26d29
GlowK <glow.k@poczta.fm>
2019-05-05 14:48:17 +0200

    Section 28

--- EJS_PostRequests/app.js
+++ EJS_PostRequests/app.js
@@ -1,6 +1,18 @@
-var express = require("express");
+const express = require("express");
 var app = express();
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+const request = require('request');
+
+
+
+// request('https://jsonplaceholder.typicode.com/users/1', (error, response, body) => {
+//     if(!error && response.statusCode == 200){
+//         const parsedData = JSON.parse(body);
+//         console.log(`${parsedData.name} lives in ${parsedData.address.city}`);
+//     }
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+// });
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -11,6 +23,22 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.get("/search", (req, res) =>{
+    res.render("search");
+});
+
+app.get("/results", (req, res) =>{
+    var query = req.query.searchFor;
+    var url = "http://www.omdbapi.com/?s="+ query +"&apikey=thewdb";
+    request( url, (error, response, body) =>{
+        if(!error && response.statusCode == 200){
+            const parsedData = JSON.parse(body);
+            // res.send(parsedData.Search[0].Title);
+            res.render("results", {data: parsedData});
+        }
+    })
+})
 
 app.post("/addfriend", function(req, res){
     var newFriend = req.body.newFriend;
@@ -25,5 +53,5 @@ app.get("/friends", function(req, res){
 })
 
 app.listen(3000, function(){
-    console.log("Server is running...");
+    console.log("########### Server is running... ###########");
 });
