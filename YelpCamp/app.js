df049adb2a50686f53f03a89ed867c9ddec44a4c
GlowK <glow.k@poczta.fm>
2019-05-05 17:35:03 +0200

    Section 29

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -12,7 +12,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 var campgrounds = [
     {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__480.jpg"},
     {name: "Granite hilll", image: "https://cdn.pixabay.com/photo/2016/11/21/16/03/campfire-1846142__480.jpg"},
+    {name: "MOntaing Goat ", image: "https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807__340.jpg"},
+    {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__480.jpg"},
+    {name: "Granite hilll", image: "https://cdn.pixabay.com/photo/2016/11/21/16/03/campfire-1846142__480.jpg"},
+    {name: "MOntaing Goat ", image: "https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807__340.jpg"},
+    {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__480.jpg"},
+    {name: "Granite hilll", image: "https://cdn.pixabay.com/photo/2016/11/21/16/03/campfire-1846142__480.jpg"},
+    {name: "MOntaing Goat ", image: "https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807__340.jpg"},
+    {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__480.jpg"},
+    {name: "Granite hilll", image: "https://cdn.pixabay.com/photo/2016/11/21/16/03/campfire-1846142__480.jpg"},
     {name: "MOntaing Goat ", image: "https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807__340.jpg"}
+
 ];
 
 // ============================
