5b3d5f6d0624d26cf5fad0014a87e51c408074fe
GlowK <glow.k@poczta.fm>
2019-05-06 23:00:48 +0200

    Section 31 Restful Blog

--- RESTful_Routing/app.js
+++ RESTful_Routing/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 var app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/BlogApp",{useNewUrlParser: true});
+app.use(methodOverride("_method"));
+mongoose.connect("mongodb://localhost:27017/BlogApp",{
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
 
 // ============================
 // Schema setup
@@ -18,11 +23,84 @@ var blogSchema = new mongoose.Schema({
     title: String,
     image: {type: String, default: basicPicture},
     body: String,
-    created: {typ: Date, default: Date.now}
+    created: {type: Date, default: Date.now}
 });
-var Campground = mongoose.model("Campground", campgroundSchema);
+var Blog = mongoose.model("Blogpost", blogSchema);
 
 
+// Blog.create({
+//     title: "Test blog",
+//     image: basicPicture,
+//     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque reprehenderit quaerat aperiam debitis voluptatibus amet maxime illo ea dolorum."
+// });
+
+// ============================
+// RESTful routes
+// ============================
+
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+// INDEX route
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) =>{
+        if(err){
+            console.log(err)
+        }else{
+            res.render("index", {blogs: blogs});
+        }
+    })
+});
+
+// NEW route
+app.get("/blogs/new", (req, res) => {
+    res.render("new");
+});
+
+// CREATE route
+app.post("/blogs", (req, res) =>{
+    Blog.create(req.body.blog, (err, newBlogPost) => {
+        if(err){
+            res.render("new");
+        }else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//SHOW route
+app.get("/blogs/:id", (req, res) => {
+    Blog.findById(req.params.id, (err, foundPost) =>{
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("show", {blog: foundPost});
+        }
+    })
+});
+
+//EDIT route
+app.get("/blogs/:id/:edit", (req, res) =>{
+    Blog.findById(req.params.id, (err, foundPost) =>{
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("edit", {blog: foundPost})
+        }
+    });
+});
+
+//UPDATE route
+app.put("/blogs/:id", (req, res) => {
+    Blog.findOneAndUpdate({_id: req.params.id}, req.body.blog, function(err, updatePost){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
 
 // ============================
 // Listen init
